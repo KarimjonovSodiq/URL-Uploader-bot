f0d6710af30830f6a8b6e4443182864db3bec3e8
CloseCoder <69109969+KarimjonovSodiq@users.noreply.github.com>
2022-03-07 20:05:53 +0500

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,6 +1,6 @@
 class Translation(object):
     START_TEXT = """Hi {},
-I'm X-URL Uploader!
+I'm CC-URL Uploader!
 You can upload HTTP/HTTPS direct link, Using this bot!
 
 /help for more details!"""
